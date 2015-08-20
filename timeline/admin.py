c30e88546c6308c0da79c9dce4dca341d5cc47df
Max Hellwig <max.hellwig@datenbetrieb.de>
2015-08-20 12:16:15 +0200

    improve admin view

--- timeline/admin.py
+++ timeline/admin.py
@@ -4,25 +4,30 @@ from .models import *
 # Register your models here.
 
 
-class MediaAdmin(admin.ModelAdmin):
+class TextAdmin(admin.ModelAdmin):
     fieldsets = [(None, {'fields': ['headline', 'text', 'slug']})]
     prepopulated_fields = {'slug': ('headline',)}
 
 
+class MediaAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['title', 'url', 'caption', 'credit', 'thumbnail', 'slug']})]
+    prepopulated_fields = {'slug': ('title',)}
+
+
 class TimelineAdmin(admin.ModelAdmin):
-    fieldsets = [(None, {'fields': ['title', 'slug', 'published']})]
+    fieldsets = [(None, {'fields': ['title', 'slug', 'published', 'options']})]
     prepopulated_fields = {'slug': ('title',)}
 
 
 class EventAdmin(admin.ModelAdmin):
     fieldsets = [(None, {'fields': ['title', 'slug', 'start_date', 'end_date', 'timeline', 'media', 'text']})]
-    list_display = ('start_date', 'end_date')
+    list_display = ('title', 'start_date', 'end_date')
     prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'start_date', 'end_date']
 
 
 admin.site.register(Timeline, TimelineAdmin)
-admin.site.register(Media)
-admin.site.register(Text)
+admin.site.register(Media, MediaAdmin)
+admin.site.register(Text, TextAdmin)
 admin.site.register(Event, EventAdmin)
-
-
+admin.site.register(Options)
