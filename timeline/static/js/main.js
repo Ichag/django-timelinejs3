f23f8601f42e94b68f34fc786814ce1fb214237d
Max Hellwig <max.hellwig@datenbetrieb.de>
2015-09-23 15:33:24 +0200

    enable datepicker for events

--- timeline/static/js/main.js
+++ timeline/static/js/main.js
@@ -1,1 +1,9 @@
-$(document).foundation();
+$(document).ready(function(){
+    $(document).foundation();
+    $('#id_start_date').fdatepicker({
+        language: 'de'
+    });
+    $('#id_end_date').fdatepicker({
+        language: 'de'
+    });
+});
